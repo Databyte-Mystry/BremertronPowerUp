6f770eb4e6471eeb74b659f96d21ddb2e4b9c45d
Databyte-Mystry <jeremy.tirador@gmail.com>
2018-02-08 16:02:01 -0800

    Ran some tests

--- myRobot2018/src/org/usfirst/frc/team3049/robot/subsystems/Gripper.java
+++ myRobot2018/src/org/usfirst/frc/team3049/robot/subsystems/Gripper.java
@@ -2,6 +2,8 @@ package org.usfirst.frc.team3049.robot.subsystems;
 
 import org.usfirst.frc.team3049.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -10,9 +12,9 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Gripper extends Subsystem {
 	public Solenoid gripper = new Solenoid(RobotMap.Gripper_Solenoid);//default should be release
-	public Solenoid pusher = new Solenoid(RobotMap.Pusher_Solenoid);//default should be retracted
-	
-    public void grip(){
+	public Solenoid pusher = new Solenoid(RobotMap.Pusher_Solenoid );//default should be retracted
+//	public DoubleSolenoid pusher = new DoubleSolenoid(RobotMap.Pusher_Solenoid, 3);//default should be retracted
+	public void grip(){
     	gripper.set(true);
     }
 
@@ -21,6 +23,14 @@ public class Gripper extends Subsystem {
     }
     
     public void push(boolean pushOn){ 
+//    	if (pushOn){
+//    		pusher.set(Value.kForward);
+//    	}else if (!pushOn){
+//    		pusher.set(Value.kReverse);
+//    	} else {
+//    		pusher.set(Value.kOff);
+//    	}
+    	
     	pusher.set(pushOn);
     }
     
